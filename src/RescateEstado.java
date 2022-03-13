9b108588a1224120b8813ce33349935340884592
ryom4 <mariofontblanc@gmail.com>
2022-03-13 12:34:43 +0100

    lib deastres

---
+++ src/RescateEstado.java
@@ -0,0 +1,23 @@
+package src;
+import aima.search.framework.HeuristicFunction;
+
+import java.util.*;
+
+/***********************************************************************
+ ***                                                                 ***
+ ***   Práctica IA Q2 / 2022                                         ***
+ ***                                                                 ***
+ ***   Alumnos: Marc Delgado Sánchez                                 ***
+ ***            Mario Font Blanc                                     ***
+ ***            Manel Piera Garrigosa                                ***
+ ***   Grupo:   14                                                   ***
+ ***                                                                 ***
+ ***   src.RescateEstado.java                                        ***
+ ***                                                                 ***
+ ***********************************************************************/
+
+public class RescateEstado {
+
+
+    public RescateEstado(int centros, int helicopteros, )
+}
