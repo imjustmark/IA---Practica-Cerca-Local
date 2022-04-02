4b3f08a3644a59278b973b006a3c757700214336
marc.delgado.sanchez <marc.delgado.sanchez@estudiant.upc.edu>
2022-04-02 18:59:56 +0200

    Funció Comprobadora Estat Final (False sempre)

---
+++ src/EstadoFinal.java
@@ -0,0 +1,10 @@
+package src;
+
+import aima.search.framework.GoalTest;
+
+public class EstadoFinal implements GoalTest {
+
+    public boolean isGoalState(Object state) {
+        return false;
+    }
+}
