86f9282ddb2dc34fd24f3cf4a8ac5b5c397f8c1a
Manel <manel.piera@estudiantat.upc.edu>
2022-04-04 17:28:22 +0200

    errorcillo

--- src/Heuristic4.java
+++ src/Heuristic4.java
@@ -4,7 +4,7 @@ import aima.search.framework.HeuristicFunction;
 
 import java.util.ArrayList;
 
-public class Heuristic3 implements HeuristicFunction {
+public class Heuristic4 implements HeuristicFunction {
     public double getHeuristicValue(Object n){
         RescateEstado state = (RescateEstado) n;
         ArrayList<ArrayList<Integer>> conf = state.getSolucion();
@@ -12,10 +12,9 @@ public class Heuristic3 implements HeuristicFunction {
         double factor = 1.0;
         int num_h = conf.size();
         for(int i = 0; i < num_h; ++i){
-            double tempsH = state.calculaTiempoH(i) + factor*state.calculaTiempoHPriod1(i);
+            double tempsH = Math.pow(state.calculaTiempoH(i) + factor*state.calculaTiempoHPriod1(i), 2);
             sum += tempsH;
         }
         return sum;
     }
 }
-
